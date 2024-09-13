7ce438fd53ddb2083a81dbe0552cf53fb36a6005
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-09-13 13:53:39 +0800

    update

--- src/acwing_user_count.py
+++ src/acwing_user_count.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 import requests
 
@@ -6,7 +7,7 @@ l, r = 0, 10 ** 7
 while l < r:
     mid = l + r + 1 >> 1
     url = 'https://www.acwing.com/user/myspace/index/' + str(mid)
-    print('mid: %d' % mid)
+    print('mid: %d,' % mid, end=' ')
     response = requests.get(url)
     if '找不到页面' in response.text:
         r = mid - 1
@@ -14,5 +15,8 @@ while l < r:
     else:
         l = mid
         print('小了')
-    time.sleep(1)
+    time.sleep(0.3)
 print('AcWing一共有%d名用户' % l)
+file = open('./acw_user_count.log', 'a')
+file.write('[' + str(datetime.now()) + ']: ' + 'AcWing一共有%d名用户\n' % l)
+file.close()
